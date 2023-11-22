fa3df96df460c003c9a85ccc6853499ecd94cd02
110а-2 <Buruburubunt@gmail.com>
2023-11-22 15:47:27 +0300

    3

--- tests/test_todomvc.py
+++ tests/test_todomvc.py
@@ -11,3 +11,12 @@ def test_complete_todo():
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
     browser.all('#todo-list>li').with_(timeout=2).should(have.size(3)) #ПрОВЕРКА ЧТО СОЗДАЛОСЬ 3 ЗАДАЧИ
+
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c')) #ПРОВЕРИТЬ ВСЕ ЗНАЧЕНИЯ
+#    browser.all('#todo-list>li').second.element('.toggle').click() #КЛИКНУТЬ ПО " ЭЛЕМЕНТУ
+    browser.all('#todo-list>li').element_by(have.exact_text('b')).element('.toggle').click() # 2 ВАРИАНТ НАПИСАНИЯ
+#    browser.element('.toggle').click() # 3 ВАРИАНТ НАПИСАНИЯ
+    browser.all('#todo-list>li').by(have.css_class('.completed')).should(have.exact_texts('b')) # НАХОДИТ ВСЕ ЭЛЕМЕНТЫ И ПРОВЕРЯЕТ ЧТО В СПИСКЕ ТОЛЬКО С ТАКИМ ТЕКСТОМ
+    browser.all('#todo-list>li').by(have.no.css_class('.completed')).should(have.exact_texts('a', 'c')) # ПРОВЕРЯЕТ ЧТО ОСТАЛЬНЫЕ ЭЛЕМЕНТЫ НЕ НАХОДЯТСЯ ПОД ЭТИМ КЛАССОМ И ИМЕЮТ ТЕКСТ А И С
+
+
